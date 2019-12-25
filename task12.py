2155fa8bdff61886dc8223cfa4990975a0bc0efe
Gulzhigit <mr.ksedos@mail.com>
2019-12-25 15:38:03 +0600

    12-commit

---
+++ task12.py
@@ -0,0 +1,8 @@
+word = input("Введите слова: ")
+if 'f' in word:
+    if word.count('f') > 1:
+        print(word.find('f'))
+        print(word.rfind('f'))
+    else:
+        print(word.find('f'))
+        
